5d1b16f1f24c6c47ae12dc5a977f5a1e1ad12354
Kyle Partridge <partkyle@gmai.com>
2021-01-10 19:34:06 -0800

    19 part 1 is functional, but it is probably a crime against nature.

--- src/bin/19_01.rs
+++ src/bin/19_01.rs
@@ -1,15 +1,74 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 use std::fs;
+
+extern crate regex;
+use regex::Regex;
 
 extern crate lib;
 use lib::error::Error;
 
-const FILENAME: &str = "files/19/sample.txt";
+const FILENAME: &str = "files/19/input.txt";
 
 #[derive(Debug)]
 enum Rule {
-    Redirection(Vec<Vec<usize>>),
+    Simple(Vec<usize>),
+    LeftRight(Box<Rule>, Box<Rule>),
     Char(String),
+}
+
+struct World<'a> {
+    start: usize,
+    rules: &'a BTreeMap<usize, Rule>,
+}
+
+impl<'a> World<'a> {
+    fn new(start: usize, rules: &'a BTreeMap<usize, Rule>) -> World {
+        World {
+            start: start,
+            rules: rules,
+        }
+    }
+
+    // fully expand a rule from a list of ids to a regex to use for parsing
+    pub fn regex(&self) -> String {
+        let start = self.rules.get(&self.start).unwrap();
+
+        let mut result = "^".to_string();
+        result += &self.regex_internal(start);
+        result += "$";
+
+        result
+    }
+
+    fn regex_internal(&self, rule: &Rule) -> String {
+        match rule {
+            Rule::Simple(list) => {
+                let mut result = "".to_string();
+                for i in list {
+                    let r = self.rules.get(&i).unwrap();
+                    result += &self.regex_internal(r);
+                }
+                result
+            }
+
+            Rule::LeftRight(left, right) => {
+                format!(
+                    "({}|{})",
+                    self.regex_internal(left),
+                    self.regex_internal(right)
+                )
+            }
+
+            Rule::Char(s) => format!("{}", s),
+
+        }
+    }
+}
+
+fn parse_simple(rule_line: &str) -> Result<Vec<usize>, Error> {
+    let ids: Result<Vec<usize>, _> = rule_line.split_whitespace().map(|i| i.parse()).collect();
+    let ids = ids?;
+    Ok(ids)
 }
 
 // parse the ruleset from an iterator of lines
@@ -47,41 +106,29 @@ where
             rules.insert(rule_no, Rule::Char(ch));
         } else {
             // Treat all Redirection types as the same. Split on the " | " to gather the separate sets of rules
-            let rule_line_parsed: Result<Vec<Vec<usize>>, _> = rule_line
-                .split(" | ")
-                .map(|s| {
-                    s.split_whitespace()
-                        .map(|i| i.parse::<usize>())
-                        .collect::<Result<Vec<usize>, _>>()
-                })
-                .collect();
-            rules.insert(rule_no, Rule::Redirection(rule_line_parsed?));
+            if rule_line.contains(" | ") {
+                let parts: Vec<&str> = rule_line.split(" | ").collect();
+
+                if parts.len() != 2 {
+                    return Err("wrong number of split parts in the | operator".into());
+                }
+
+                rules.insert(
+                    rule_no,
+                    Rule::LeftRight(
+                        Box::new(Rule::Simple(parse_simple(parts[0])?)),
+                        Box::new(Rule::Simple(parse_simple(parts[1])?)),
+                    ),
+                );
+            // TODO: this only handles 2
+            } else {
+                rules.insert(rule_no, Rule::Simple(parse_simple(rule_line)?));
+            }
         }
     }
 
     Ok(rules)
 }
-
-// fn expand_rule(n: usize, rules: &BTreeMap<usize, Rule>) -> Result<Vec<String>, Error> {
-//     let start = rules.get(&n).expect("none found");
-
-//     match start {
-//         Rule::Redirection(sets) => {
-//             let mut results = vec![""];
-//             for set in sets {
-//                 for &n in set {
-//                     for expanded in expand_rule(n, &rules)? {
-//                         let result = "".to_string();
-//                         results.iter().map(|s| s + expanded)
-//                     }
-//                 }
-//             }
-
-//             return Ok(results);
-//         }
-//         Rule::Char(s) => return Ok(vec![s.to_string()]),
-//     }
-// }
 
 fn main() -> Result<(), Error> {
     let input = fs::read_to_string(FILENAME)?;
@@ -90,19 +137,12 @@ fn main() -> Result<(), Error> {
 
     let rules = parse_ruleset(&mut line_iter)?;
 
-    println!("{:?}", rules);
+    let world = World::new(0, &rules);
 
-    // let expanded = expand_rule(0, &rules);
-    // println!("{:?}", expanded);
+    let re = Regex::new(&world.regex()).map_err(|_e| "nothing")?;
 
-    // let's try creating all allowable strings based on rule 0
-    // and storing them in a set
-    // this might not work for large datasets
-
-    // the remainder of the lines to be compared
-    while let Some(line) = line_iter.next() {
-        println!("{:?}", line);
-    }
+    let count = line_iter.filter(|line| re.is_match(line)).count();
+    println!("answer: {}", count);
 
     Ok(())
 }
