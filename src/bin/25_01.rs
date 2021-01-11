94aa6294ad1be2810aeb3a93d2db2465f84e3c92
Kyle Partridge <partkyle@gmai.com>
2021-01-10 17:32:41 -0800

    make find_loop safer

--- src/bin/25_01.rs
+++ src/bin/25_01.rs
@@ -1,35 +1,39 @@
+// transform a value based on the formula:
+//  - Set the value to itself multiplied by the subject number.
+//  - Set the value to the remainder after dividing the value by 20201227.
 fn transform_subject(current: usize, subject: usize) -> usize {
     (current * subject) % 20201227
 }
 
-fn find_loop(n: usize) -> usize {
+fn find_loop(n: usize) -> Option<usize> {
     let subject = 7;
     let mut current = 1;
-    let mut i = 0;
+    let mut i: usize = 0;
 
-    // this can technically loop forever, so we probably want a way to stop that
-    // and potentially return an Option<usize> instead
     while current != n {
         current = transform_subject(current, subject);
-        i += 1;
+        // use a checked add to prevent overflow issues
+        // (just for completeness, they don't happen in this sample set)
+        i = i.checked_add(1)?
     }
 
-    i
+    Some(i)
 }
 
 /*
+input:
+
 9232416
 14144084
 */
-
 fn main() {
     let card_public = 9232416;
     let door_public = 14144084;
 
     // it's not efficient to do this twice since the calculations are the same
     // so we should optimize that if it's important.
-    let card_loop = find_loop(card_public);
-    let door_loop = find_loop(door_public);
+    let card_loop = find_loop(card_public).expect("card public key loop size was not found");
+    let door_loop = find_loop(door_public).expect("door public key loop size was not found");
 
     // card: 8927518 door: 13240670
     println!("card: {} door: {}", card_loop, door_loop);
